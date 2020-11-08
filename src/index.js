b3e163371f1e2689727a105f5d17bac52dc42e23
charleshugochg <minheinkhant@hotmail.com>
2020-11-08 14:14:33 +0630

    make ready for production

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 const authorizer = require("./graphql/authorizer");
 
-const { MONGODB_URL } = require("../config");
+const { MONGODB_URL } = require("../config") || process.env;
 
 const server = new ApolloServer({
   typeDefs,
@@ -17,8 +17,11 @@ mongoose
   .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("MongoDB connected.");
-    return server.listen();
+    return server.listen({ port: process.env.PORT || 4000 });
   })
   .then((res) => {
     console.log(`Server is listening at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
   });
